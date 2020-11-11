567c7aeb04a4610d9f4cb8d0cb057b058419d255
wrefgtzweve <69946827+wrefgtzweve@users.noreply.github.com>
2020-11-11 02:42:36 +0100

    Add concmd

---
+++ lua/autorun/client/cl_cfc_loadout.lua
@@ -0,0 +1,38 @@
+local uiColor = Color( 36, 41, 67, 255 )
+
+local function openLoadout()
+    local window = vgui.Create( "DFrame" )
+
+    if ScrW() > 640 then
+        window:SetSize( ScrW() * 0.6, ScrH() * 0.6 )
+    else
+        window:SetSize( 640, 480 )
+    end
+
+    window:Center()
+    window:SetTitle( "CFC Loadout" )
+    window:MakePopup()
+
+    window.Paint = function( self, w, h )
+        draw.RoundedBox( 8, 0, 0, w, h, uiColor )
+    end
+
+    local button = vgui.Create( "DButton", window )
+	button:SetText( "Close" )
+	button.DoClick = function() window:Close() end
+	button:SetSize( 100, 40 )
+	button:SetPos( (window:GetWide() - button:GetWide()) / 2, window:GetTall() - button:GetTall() - 10 )
+
+end
+
+concommand.Add( "cfc_loadout", openLoadout )
+
+hook.Add( "OnPlayerChat", "CFC_Loadout_OpenLoadoutCommand", function( ply, msg )
+    if not string.StartWith( msg, "!loadout" ) then return end
+
+    if ply == LocalPlayer() then
+        openLoadout()
+    end
+
+    return true
+end )
