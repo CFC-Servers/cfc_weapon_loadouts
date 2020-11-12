e9d566f4292b39fe0deb3a99bc7cc9c6c531d698
wrefgtzweve <69946827+wrefgtzweve@users.noreply.github.com>
2020-11-12 04:00:36 +0100

    Added panels Fix weapon add

--- lua/autorun/client/cl_cfc_loadout.lua
+++ lua/autorun/client/cl_cfc_loadout.lua
@@ -1,10 +1,11 @@
 local uiColor = Color( 36, 41, 67, 255 )
 
 local function openLoadout()
+    -- Window init
     local window = vgui.Create( "DFrame" )
 
     if ScrW() > 640 then
-        window:SetSize( ScrW() * 0.3, ScrH() * 0.3 )
+        window:SetSize( ScrW() * 0.5, ScrH() * 0.5 )
     else
         window:SetSize( 640, 480 )
     end
@@ -16,9 +17,26 @@ local function openLoadout()
     window.Paint = function( self, w, h )
         draw.RoundedBox( 8, 0, 0, w, h, uiColor )
     end
+    -- Sheet and Panels
 
-    local weaponList = vgui.Create ( "DListView" , window )
-    weaponList:SetPos( 10, 30 )
+    local sheet = vgui.Create( "DPropertySheet", window )
+    sheet.Paint = function( self, w, h )  draw.RoundedBox( 4, 0, 0, w, h, Color( 169, 176, 214, self:GetAlpha() ) ) end 
+    sheet:Dock( FILL )
+
+    local panel1 = vgui.Create( "DPanel", sheet )
+    panel1.Paint = function( self, w, h ) end 
+    sheet:AddSheet( "test", panel1, "icon16/cross.png" )
+
+    local panel2 = vgui.Create( "DPanel", sheet )
+    panel2.Paint = function( self, w, h )  end 
+    sheet:AddSheet( "test 2", panel2, "icon16/tick.png" )
+
+    -- Panel 1 panel1
+
+    -- Panel 2 panel2
+
+        local weaponList = vgui.Create ( "DListView" , panel2 )
+    weaponList:SetPos( 20, window:GetTall() - 470 )
     weaponList:SetSize( 150, window:GetTall() - 40 )
     weaponList:AddColumn( "Selected Weapons" )
 
@@ -26,21 +44,22 @@ local function openLoadout()
         weaponList:RemoveLine( line )
     end
 
-    local presetList = vgui.Create ( "DListView" , window )
-    presetList:SetPos( window:GetWide() - 160, 30 )
+    local presetList = vgui.Create ( "DListView" , panel2 )
+    presetList:SetPos( window:GetWide() - 160, 40 )
     presetList:SetSize( 150, window:GetTall() - 40 )
     presetList:AddColumn( "Saved Presets" )
 
-    local weaponEntry = vgui.Create ( "DTextEntry" , window )
+    local weaponEntry = vgui.Create ( "DTextEntry" , panel2 )
     weaponEntry:SetPos( window:GetWide() / 2 - 100, 30 )
     weaponEntry:SetSize( 200, 20 )
 
-    local weaponAddButton = vgui.Create( "DButton", window )
+    local weaponAddButton = vgui.Create( "DButton", panel2 )
     weaponAddButton:SetPos( window:GetWide() / 2 - 100,60 )
     weaponAddButton:SetSize( 200, 20 )
     weaponAddButton:SetText( "Add weapon" )
     weaponAddButton.DoClick = function()
-        if weapons.Get( weaponEntry:GetValue() ) ~= nil then
+        local weaponClass = ents.FindByClass( weaponEntry:GetValue() )
+        if weaponClass[1] ~= nil then
             weaponList:AddLine( weaponEntry:GetValue() )
         else
             weaponAddButton:SetText( "Please enter a valid weapon." )
@@ -51,6 +70,8 @@ local function openLoadout()
             end)
         end
     end
+
+    -- le funny button
 
     local button = vgui.Create( "DButton", window )
     button:SetText( "Close" )
