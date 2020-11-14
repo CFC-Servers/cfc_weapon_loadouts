f30c4adce301e78e139a8ed1958288e92da45ec0
wrefgtzweve <69946827+wrefgtzweve@users.noreply.github.com>
2020-11-14 04:03:08 +0100

    Shape change

--- lua/autorun/client/cl_cfc_loadout.lua
+++ lua/autorun/client/cl_cfc_loadout.lua
@@ -211,21 +211,22 @@ function createWeaponIcon ( X, Y, ent )
 
     weaponIcon.selectionShape = vgui.Create( "DShape", panel3 )
     weaponIcon.selectionShape:SetType( "Rect" ) -- This is the only type it can be
-    weaponIcon.selectionShape:SetPos( 60, 5 )
-    weaponIcon.selectionShape:SetColor( Color(0, 255, 0, 5) )
-    weaponIcon.selectionShape:SetSize( 12, 12 )
+    weaponIcon.selectionShape:SetPos( 5, 5 )
+    weaponIcon.selectionShape:SetParent( weaponIcon )
+    weaponIcon.selectionShape:SetColor( Color(0, 255, 0, 100) )
+    weaponIcon.selectionShape:SetSize( 12, 120 )
 
     weaponIcon.DoClick = function()
         if weaponIcon.Selected == false then
             weaponIcon.Selected = true
             table.insert( currentSelectionWeapons, weaponIcon.weaponClass )
         else
-            weaponIcon.Selected = true
             for I, value in pairs( currentSelectionWeapons ) do
                 if value ~= weaponIcon.weaponClass then return end
                 table.remove( currentSelectionWeapons, I )
                 break
             end
+            weaponIcon.Selected = false
         end
         populateWeaponList()
     end
