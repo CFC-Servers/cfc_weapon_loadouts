ed03e0cbbac0ee97d9ddbe6b38db9bceed9c456c
wrefgtzweve <69946827+wrefgtzweve@users.noreply.github.com>
2020-12-25 23:47:18 +0100

    Add weapon spawning and ammo set

--- lua/autorun/server/sv_cfc_loadout.lua
+++ lua/autorun/server/sv_cfc_loadout.lua
@@ -1,6 +1,20 @@
 util.AddNetworkString( "CFC_Loadout_WeaponTable" )
 util.AddNetworkString( "CFC_Loadout_Resetweapons" )
 
+local function giveWeapons( ply )
+    if ply.loadoutWeapons == nil then return end
+    for _, weapon in pairs( ply.loadoutWeapons ) do
+        local canSpawn = hook.Run( "PlayerGiveSWEP", ply, weapon )
+        if canSpawn == true then
+            ply:Give( weapon )
+            local weaponEnt = ply:GetWeapon( weapon )
+            local ammoType = weaponEnt:GetPrimaryAmmoType()
+            ply:SetAmmo( 1000, ammoType )
+        end
+    end
+    return true
+end
+
 net.Receive( "CFC_Loadout_WeaponTable", function( _, ply )
     local weaponTable = net.ReadTable()
     ply.loadoutWeapons = weaponTable
@@ -8,13 +22,6 @@ end )
 
 net.Receive( "CFC_Loadout_Resetweapons", function( _, ply )
     ply.loadoutWeapons = nil
-    PrintTable(ply.loadoutWeapons)
 end )
 
-hook.Add( "PlayerLoadout", "PlayerSpawnWeaponsLoadout", function( ply )
-    if not ply.loadoutWeapons then return false end
-    for _, weapon in pairs( ply.loadoutWeapons ) do
-        hook.Run( "PlayerGiveSWEP", ply, weapon )
-    end
-    return true
-end)
+hook.Add( "PlayerLoadout", "PlayerSpawnWeaponsLoadout", giveWeapons , HOOK_HIGH )
