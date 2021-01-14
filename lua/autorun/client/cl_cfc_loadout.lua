f4fa1211191327fbac2d54b5480a07c6c4298119
wrefgtzweve <69946827+wrefgtzweve@users.noreply.github.com>
2021-01-14 18:15:30 +0100

    Change window size

--- lua/autorun/client/cl_cfc_loadout.lua
+++ lua/autorun/client/cl_cfc_loadout.lua
@@ -44,7 +44,7 @@ end )
 local function openLoadout()
     -- Window init
     window = vgui.Create( "DFrame" )
-    window:SetSize( 1024, 768 )
+    window:SetSize( 1280, 720 )
     window:Center()
     window:SetTitle( "CFC Loadout" )
     window:SetDeleteOnClose( false )
