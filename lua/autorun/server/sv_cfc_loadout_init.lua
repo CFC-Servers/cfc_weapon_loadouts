063589c43d211a8dfa4e02ff493f5a46caa6947a
wrefgtzweve <69946827+wrefgtzweve@users.noreply.github.com>
2021-01-19 20:55:55 +0100

    WIP file structure change

---
+++ lua/autorun/server/sv_cfc_loadout_init.lua
@@ -0,0 +1,6 @@
+util.AddNetworkString( "CFC_Loadout_WeaponTable" )
+util.AddNetworkString( "CFC_Loadout_Resetweapons" )
+util.AddNetworkString( "CFC_Loadout_InitialSpawn" )
+util.AddNetworkString( "CFC_Loadout_SendRestrictions" )
+
+include( "cfc_loadouts/server/sv_cfc_loadout.lua" )
