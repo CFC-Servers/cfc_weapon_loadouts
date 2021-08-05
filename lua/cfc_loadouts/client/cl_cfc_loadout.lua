0d305794de6e7854377df1018c7fe153070b7a5f
wrefgtzweve <69946827+wrefgtzweve@users.noreply.github.com>
2021-08-05 14:10:13 +0200

    Fix my fucking ugly old stylin

--- lua/cfc_loadouts/client/cl_cfc_loadout.lua
+++ lua/cfc_loadouts/client/cl_cfc_loadout.lua
@@ -2,15 +2,15 @@ local weaponCategorised = { }
 local allWeapons = { }
 file.CreateDir( "cfc_loadout" )
 
-hook.Add( "InitPostEntity", "CFC_Loadouts_ReadyClientCheck", function( )
+hook.Add( "InitPostEntity", "CFC_Loadouts_ReadyClientCheck", function()
     net.Start( "CFC_Loadout_InitialSpawn" )
-    net.SendToServer( )
+    net.SendToServer()
 end )
 
-net.Receive( "CFC_Loadout_SendRestrictions", function( )
+net.Receive( "CFC_Loadout_SendRestrictions", function()
     allWeapons = list.Get( "Weapon" )
-    local group = LocalPlayer( ):GetUserGroup( )
-    local weaponTable = net.ReadTable( )
+    local group = LocalPlayer():GetUserGroup()
+    local weaponTable = net.ReadTable()
 
     for _, weapon in pairs( allWeapons ) do
         local weaponClass = weapon.ClassName
@@ -29,12 +29,12 @@ net.Receive( "CFC_Loadout_SendRestrictions", function( )
 end )
 
 -- Derma stuff
-function CFCLoadouts.openLoadout( )
+function CFCLoadouts.openLoadout()
     local window
 
     if window then
-        window:Show( )
-        window:MakePopup( )
+        window:Show()
+        window:MakePopup()
 
         return
     end
@@ -42,10 +42,10 @@ function CFCLoadouts.openLoadout( )
     -- Window init
     window = vgui.Create( "DFrame" )
     window:SetSize( 800, 600 )
-    window:Center( )
+    window:Center()
     window:SetTitle( "CFC Loadout" )
     window:SetDeleteOnClose( false )
-    window:MakePopup( )
+    window:MakePopup()
 
     window.Paint = function( _, w, h )
         draw.RoundedBox( 8, 0, 0, w, h, Color( 36, 41, 67, 255 ) )
@@ -66,7 +66,7 @@ function CFCLoadouts.openLoadout( )
     end
 
     panel1sheet.Tab.Paint = function( self, w, h )
-        if sheet:GetActiveTab( ) == self then
+        if sheet:GetActiveTab() == self then
             draw.RoundedBox( 0, 0, 0, w, h, Color( 50, 58, 103, 255 ) )
         else
             draw.RoundedBox( 0, 0, 0, w, h, Color( 40, 48, 93, 255 ) )
@@ -81,7 +81,7 @@ function CFCLoadouts.openLoadout( )
     end
 
     panel2sheet.Tab.Paint = function( self, w, h )
-        if sheet:GetActiveTab( ) == self then
+        if sheet:GetActiveTab() == self then
             draw.RoundedBox( 0, 0, 0, w, h, Color( 50, 58, 103, 255 ) )
         else
             draw.RoundedBox( 0, 0, 0, w, h, Color( 40, 48, 93, 255 ) )
@@ -95,26 +95,26 @@ function CFCLoadouts.openLoadout( )
     -- Panel 1 panel1   ---
     -----------------------
     local weaponLoadoutPreview = vgui.Create( "DPanel", panel1 )
-    weaponLoadoutPreview:SetPos( ScrW( ) * 0.0825, ScrH( ) * 0.01 )
-    weaponLoadoutPreview:SetSize( ScrW( ) * 0.3225, ScrH( ) * 0.4875 )
+    weaponLoadoutPreview:SetPos( ScrW() * 0.0825, ScrH() * 0.01 )
+    weaponLoadoutPreview:SetSize( ScrW() * 0.3225, ScrH() * 0.4875 )
 
     weaponLoadoutPreview.Paint = function( _, w, h )
         draw.RoundedBox( 0, 0, 0, w, h, Color( 42, 47, 74, 255 ) )
     end
 
     local weaponLoadoutPreviewScroll = vgui.Create( "DScrollPanel", panel1 )
-    weaponLoadoutPreviewScroll:SetPos( ScrW( ) * 0.0825, ScrH( ) * 0.01 )
-    weaponLoadoutPreviewScroll:SetSize( ScrW( ) * 0.325, ScrH( ) * 0.5 )
-    weaponLoadoutPreviewScroll:Hide( )
+    weaponLoadoutPreviewScroll:SetPos( ScrW() * 0.0825, ScrH() * 0.01 )
+    weaponLoadoutPreviewScroll:SetSize( ScrW() * 0.325, ScrH() * 0.5 )
+    weaponLoadoutPreviewScroll:Hide()
     local loadoutPreviewList = vgui.Create( "DListView", panel1 )
-    loadoutPreviewList:SetPos( ScrW( ) * 0.005, ScrH( ) * 0.01 )
-    loadoutPreviewList:SetSize( ScrW( ) * 0.075, ScrH( ) * 0.4325 )
+    loadoutPreviewList:SetPos( ScrW() * 0.005, ScrH() * 0.01 )
+    loadoutPreviewList:SetSize( ScrW() * 0.075, ScrH() * 0.4325 )
     loadoutPreviewList:SetMultiSelect( false )
     loadoutPreviewList:AddColumn( "Loadouts" )
 
     loadoutPreviewList.OnRowSelected = function( _, _, line )
-        weaponLoadoutPreview:Clear( )
-        weaponLoadoutPreviewScroll:Clear( )
+        weaponLoadoutPreview:Clear()
+        weaponLoadoutPreviewScroll:Clear()
         local weaponTable = CFCLoadouts.getLoadoutJsonTable( line:GetValue( 1 ) )
         local panelToUse
         if weaponTable == nil then return end
@@ -123,10 +123,10 @@ function CFCLoadouts.openLoadout( )
             panelToUse = weaponLoadoutPreviewScroll
         else
             panelToUse = weaponLoadoutPreview
-            weaponLoadoutPreviewScroll:Hide( )
+            weaponLoadoutPreviewScroll:Hide()
         end
 
-        panelToUse:Show( )
+        panelToUse:Show()
         local X = 5
         local Y = 5
         local lastWep = ""
@@ -148,29 +148,29 @@ function CFCLoadouts.openLoadout( )
     end
 
     local loadoutSelectButton = vgui.Create( "DButton", panel1 )
-    loadoutSelectButton:SetPos( ScrW( ) * 0.005, ScrH( ) * 0.445 )
-    loadoutSelectButton:SetSize( ScrW( ) * 0.075, ScrH( ) * 0.025 )
+    loadoutSelectButton:SetPos( ScrW() * 0.005, ScrH() * 0.445 )
+    loadoutSelectButton:SetSize( ScrW() * 0.075, ScrH() * 0.025 )
     loadoutSelectButton:SetText( "Select loadout" )
     CFCLoadouts.paintButton( loadoutSelectButton )
 
-    loadoutSelectButton.DoClick = function( )
-        local _, line = loadoutPreviewList:GetSelectedLine( )
+    loadoutSelectButton.DoClick = function()
+        local _, line = loadoutPreviewList:GetSelectedLine()
         if line == nil then return end
         local selectedWeapons = CFCLoadouts.getLoadoutJsonTable( line:GetValue( 1 ) )
         net.Start( "CFC_Loadout_WeaponTable" )
         net.WriteTable( selectedWeapons )
-        net.SendToServer( )
+        net.SendToServer()
     end
 
     local resetSelectButton = vgui.Create( "DButton", panel1 )
-    resetSelectButton:SetPos( ScrW( ) * 0.005, ScrH( ) * 0.4725 )
-    resetSelectButton:SetSize( ScrW( ) * 0.075, ScrH( ) * 0.025 )
+    resetSelectButton:SetPos( ScrW() * 0.005, ScrH() * 0.4725 )
+    resetSelectButton:SetSize( ScrW() * 0.075, ScrH() * 0.025 )
     resetSelectButton:SetText( "Select default loadout" )
     CFCLoadouts.paintButton( resetSelectButton )
 
-    resetSelectButton.DoClick = function( )
+    resetSelectButton.DoClick = function()
         net.Start( "CFC_Loadout_Resetweapons" )
-        net.SendToServer( )
+        net.SendToServer()
     end
 
     -----------------------
@@ -178,8 +178,8 @@ function CFCLoadouts.openLoadout( )
     -----------------------
     local weaponIcons = { }
     local loadoutListEditor = vgui.Create( "DListView", panel2 )
-    loadoutListEditor:SetPos( ScrW( ) * 0.005, ScrH( ) * 0.01 )
-    loadoutListEditor:SetSize( ScrW( ) * 0.075, ScrH( ) * 0.4 )
+    loadoutListEditor:SetPos( ScrW() * 0.005, ScrH() * 0.01 )
+    loadoutListEditor:SetSize( ScrW() * 0.075, ScrH() * 0.4 )
     loadoutListEditor:SetMultiSelect( false )
     loadoutListEditor:AddColumn( "Saved Loadouts" )
 
@@ -189,7 +189,7 @@ function CFCLoadouts.openLoadout( )
         if weaponTable == nil then
             for weaponString in pairs( weaponIcons ) do
                 local icon = weaponIcons[ weaponString ]
-                icon:Hide( )
+                icon:Hide()
             end
 
             return
@@ -208,9 +208,9 @@ function CFCLoadouts.openLoadout( )
             local icon = weaponIcons[ weaponString ]
 
             if not weaponTable[ weaponString ] then
-                icon:Show( )
+                icon:Show()
             else
-                icon:Hide( )
+                icon:Hide()
             end
         end
     end
@@ -226,29 +226,29 @@ function CFCLoadouts.openLoadout( )
     end
 
     local saveLoadoutButton = vgui.Create( "DButton", panel2 )
-    saveLoadoutButton:SetPos( ScrW( ) * 0.005, ScrH( ) * 0.4125 )
-    saveLoadoutButton:SetSize( ScrW( ) * 0.075, ScrH( ) * 0.02 )
+    saveLoadoutButton:SetPos( ScrW() * 0.005, ScrH() * 0.4125 )
+    saveLoadoutButton:SetSize( ScrW() * 0.075, ScrH() * 0.02 )
     saveLoadoutButton:SetText( "Save to selected" )
     CFCLoadouts.paintButton( saveLoadoutButton )
 
-    saveLoadoutButton.DoClick = function( )
-        CFCLoadouts.confirmationPopup( "Save loadout", "Do you want to overwrite this loadout?", false, function( )
+    saveLoadoutButton.DoClick = function()
+        CFCLoadouts.confirmationPopup( "Save loadout", "Do you want to overwrite this loadout?", false, function()
             local weaponsTable = CFCLoadouts.getSelectedWeapons( weaponIcons )
-            local _, saveLine = loadoutListEditor:GetSelectedLine( )
+            local _, saveLine = loadoutListEditor:GetSelectedLine()
             if saveLine == nil then return end
             CFCLoadouts.loadoutFileSave( saveLine:GetValue( 1 ), weaponsTable )
         end )
     end
 
     local renameLoadoutButton = vgui.Create( "DButton", panel2 )
-    renameLoadoutButton:SetPos( ScrW( ) * 0.005, ScrH( ) * 0.435 )
-    renameLoadoutButton:SetSize( ScrW( ) * 0.075, ScrH( ) * 0.02 )
+    renameLoadoutButton:SetPos( ScrW() * 0.005, ScrH() * 0.435 )
+    renameLoadoutButton:SetSize( ScrW() * 0.075, ScrH() * 0.02 )
     renameLoadoutButton:SetText( "Rename selected" )
     CFCLoadouts.paintButton( renameLoadoutButton )
 
-    renameLoadoutButton.DoClick = function( )
+    renameLoadoutButton.DoClick = function()
         CFCLoadouts.confirmationPopup( "Rename loadout", "Please enter a new name for the loadout.", true, function( textEntryValue )
-            local _, renameLine = loadoutListEditor:GetSelectedLine( )
+            local _, renameLine = loadoutListEditor:GetSelectedLine()
             if renameLine == nil then return end
             CFCLoadouts.loadoutFileRename( renameLine:GetValue( 1 ), textEntryValue )
             CFCLoadouts.loadoutFileCheck( dlistFiles )
@@ -256,12 +256,12 @@ function CFCLoadouts.openLoadout( )
     end
 
     local newLoadoutButton = vgui.Create( "DButton", panel2 )
-    newLoadoutButton:SetPos( ScrW( ) * 0.005, ScrH( ) * 0.4575 )
-    newLoadoutButton:SetSize( ScrW( ) * 0.075, ScrH( ) * 0.02 )
+    newLoadoutButton:SetPos( ScrW() * 0.005, ScrH() * 0.4575 )
+    newLoadoutButton:SetSize( ScrW() * 0.075, ScrH() * 0.02 )
     newLoadoutButton:SetText( "Create new" )
     CFCLoadouts.paintButton( newLoadoutButton )
 
-    newLoadoutButton.DoClick = function( )
+    newLoadoutButton.DoClick = function()
         CFCLoadouts.confirmationPopup( "New loadout", "Please enter a name for the new loadout.", true, function( textEntryValue )
             local weaponsTable = CFCLoadouts.getSelectedWeapons( weaponIcons )
             CFCLoadouts.loadoutFileCreate( textEntryValue, weaponsTable )
@@ -270,15 +270,15 @@ function CFCLoadouts.openLoadout( )
     end
 
     local deleteLoadoutButton = vgui.Create( "DButton", panel2 )
-    deleteLoadoutButton:SetPos( ScrW( ) * 0.005, ScrH( ) * 0.48 )
-    deleteLoadoutButton:SetSize( ScrW( ) * 0.075, ScrH( ) * 0.02 )
+    deleteLoadoutButton:SetPos( ScrW() * 0.005, ScrH() * 0.48 )
+    deleteLoadoutButton:SetSize( ScrW() * 0.075, ScrH() * 0.02 )
     deleteLoadoutButton:SetText( "Delete selected" )
     CFCLoadouts.paintButton( deleteLoadoutButton )
 
-    deleteLoadoutButton.DoClick = function( )
-        CFCLoadouts.confirmationPopup( "Delete loadout", "Are you sure you want to delete the selected loadout?", false, function( )
+    deleteLoadoutButton.DoClick = function()
+        CFCLoadouts.confirmationPopup( "Delete loadout", "Are you sure you want to delete the selected loadout?", false, function()
             for _, lineDel in pairs( loadoutListEditor.Lines ) do
-                if lineDel:IsLineSelected( ) then
+                if lineDel:IsLineSelected() then
                     CFCLoadouts.loadoutFileDelete( lineDel:GetValue( 1 ) )
                 end
             end
@@ -288,10 +288,10 @@ function CFCLoadouts.openLoadout( )
     end
 
     local scrollDock = vgui.Create( "DScrollPanel", panel2 )
-    scrollDock:SetPos( ScrW( ) * 0.0825, ScrH( ) * 0.01 )
-    scrollDock:SetSize( ScrW( ) * 0.325, ScrH( ) * 0.49 )
+    scrollDock:SetPos( ScrW() * 0.0825, ScrH() * 0.01 )
+    scrollDock:SetSize( ScrW() * 0.325, ScrH() * 0.49 )
     local weaponCats = vgui.Create( "DListLayout", scrollDock )
-    weaponCats:SetSize( ScrW( ) * 0.317, ScrH( ) * 0.4875 )
+    weaponCats:SetSize( ScrW() * 0.317, ScrH() * 0.4875 )
 
     for catName, v in SortedPairs( weaponCategorised ) do
         local X = 0
@@ -303,7 +303,7 @@ function CFCLoadouts.openLoadout( )
         weaponCatPanel.Paint = function( self, w, h )
             draw.RoundedBox( 0, 0, 0, w, h, Color( 36, 41, 67, 255 ) )
 
-            if self:IsHovered( ) then
+            if self:IsHovered() then
                 draw.RoundedBox( 0, 1, 1, w - 2, h - 2, Color( 35, 42, 69, 255 ) )
             else
                 draw.RoundedBox( 0, 1, 1, w - 2, h - 2, Color( 42, 47, 74, 255 ) )
@@ -330,8 +330,8 @@ concommand.Add( "cfc_loadout", CFCLoadouts.openLoadout )
 hook.Add( "OnPlayerChat", "CFC_Loadout_OpenLoadoutCommand", function( ply, msg )
     if not string.StartWith( msg, "!loadout" ) then return end
 
-    if ply == LocalPlayer( ) then
-        CFCLoadouts.openLoadout( )
+    if ply == LocalPlayer() then
+        CFCLoadouts.openLoadout()
     end
 
     return true
